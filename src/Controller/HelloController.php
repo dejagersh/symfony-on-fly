41711950a3440b940493d821fee9a37bf1c97713
Johan de Jager <johan@laptify.nl>
2022-10-23 22:33:50 +0200

    hehe

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -3,16 +3,24 @@
 namespace App\Controller;
 
 use App\Entity\Product;
+use App\Message\SendEmailMessage;
 use Doctrine\Persistence\ManagerRegistry;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Messenger\Transport\TransportInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HelloController extends AbstractController
 {
     #[Route('/')]
-    public function index(ManagerRegistry $doctrine)
+    public function index(LoggerInterface $logger, ManagerRegistry $doctrine, MessageBusInterface $messageBus)
     {
+        $logger->error('Hello, world');
+
+        $messageBus->dispatch(new SendEmailMessage('Johannes'));
+
         $entityManager = $doctrine->getManager();
 
         $product = new Product();
